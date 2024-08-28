a821db846eb365e55701867af236edb49c3f43f1
nmTTT <nomttumurkhuu@yahoo.com>
2024-08-28 12:57:12 +0800

    nemelt

--- server_express_tracker/server.js
+++ server_express_tracker/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const { logger } = require("./middleware/logger");
 dotenv.config();
 
 const PORT = process.env.PORT;
@@ -9,18 +10,23 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(logger());
 
 app.get("/user", (req, res) => {
   console.log("all user is read successfully");
+  res.json({ message: "GET success" });
 });
 app.post("/user", (req, res) => {
   console.log("New user is created successfully");
+  res.json({ message: "POST success" });
 });
 app.put("/user", (req, res) => {
   console.log("user is updated successfully");
+  res.json({ message: "UPDATE success" });
 });
 app.delete("/user", (req, res) => {
   console.log("user is deleted successfully");
+  res.json({ message: "DELETE success" });
 });
 
 app.get("/category", (req, res) => {
